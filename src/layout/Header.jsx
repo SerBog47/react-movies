ee90617de3bc21bf54e353d09d713f16dbc45fb8
Sergey Bogomolov <Bogomolov15@yandex.ru>
2022-10-20 17:12:10 +0300

    project complete

---
+++ src/layout/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+ return (
+    <nav className="teal darken-4">
+    <div className="nav-wrapper">
+      <a href="!#" className="brand-logo">Movies</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="!#">Фильмы</a></li>
+        <li><a href="!#">Сериалы</a></li>
+      </ul>
+    </div>
+  </nav>
+ )
+}
+
+export {Header}
